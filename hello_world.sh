ec7234e9958273ba7c218442fa6443c8b962402b
jsriveraa1 <juan.sebastian.rivera.abreo@gmail.com>
2023-08-22 15:24:43 -0500

    Update hello_world.sh
    fix

--- hello_world.sh
+++ hello_world.sh
@@ -4,7 +4,7 @@ echo "Hello world"
 
 pwd
 
-if grep -w "#<p>hola</p>" ./prueba.html ; then
+if grep -w "#<p>hola</p>" ./prueba/prueba.html ; then
     resultado= "Entro"
     grep -w "<p>hola</p>" ./prueba.html
 fi
